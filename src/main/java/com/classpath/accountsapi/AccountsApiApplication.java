0b06ee9ba8d51f96b20ce65fcf585f24cb1d1b1e
pradeep <pradeep@classpath.io>
2021-08-31 12:24:31 +0530

    committing the management endpoints

--- src/main/java/com/classpath/accountsapi/AccountsApiApplication.java
+++ src/main/java/com/classpath/accountsapi/AccountsApiApplication.java
@@ -2,10 +2,15 @@ package com.classpath.accountsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 @SpringBootApplication
 public class AccountsApiApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(AccountsApiApplication.class, args);
+		SpringApplication application = new SpringApplication(AccountsApiApplication.class);
+		//SpringApplication.run(AccountsApiApplication.class, args);
+		application.setApplicationStartup(new BufferingApplicationStartup(1500));
+		application.run(args);
+
 	}
 }
