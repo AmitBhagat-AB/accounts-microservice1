497161162b537b20a5dd55c5cac592bdc2994915
pradeep <pradeep@classpath.io>
2021-09-03 10:55:31 +0530

    committing the changes

--- src/main/java/com/classpath/accountsapi/AccountsApiApplication.java
+++ src/main/java/com/classpath/accountsapi/AccountsApiApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableResourceServer
 public class AccountsApiApplication {
 	public static void main(String[] args) {
 		SpringApplication application = new SpringApplication(AccountsApiApplication.class);
---
+++ src/main/java/com/classpath/accountsapi/config/ApplicationSecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.classpath.accountsapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+public class ApplicationSecurityConfiguration extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/accounts/**")
+                    .hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.POST, "/api/accounts/**")
+                    .hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/api/accounts/**")
+                    .hasRole("ADMIN")
+                .anyRequest()
+                .authenticated();
+    }
+}
